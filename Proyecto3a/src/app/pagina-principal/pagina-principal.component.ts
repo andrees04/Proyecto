32e39040a394dedd7fe5b09e0a3d50ae3032090c
andrees04 <blasco.ivia@gmail.com>
2024-04-30 19:54:18 +0200

    Botones de imgenes Pagina Principal Hechos, Falta alinear

--- Proyecto3a/src/app/pagina-principal/pagina-principal.component.ts
+++ Proyecto3a/src/app/pagina-principal/pagina-principal.component.ts
@@ -5,6 +5,7 @@ import { EncabezadoComponent } from '../encabezado/encabezado.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-pagina-principal',
@@ -14,8 +15,14 @@ import { NgModule } from '@angular/core';
   styleUrl: './pagina-principal.component.css'
 })
 export class PaginaPrincipalComponent {
-  showIcon: boolean = false;
+
+  mostrarFondo: boolean = false;
 
   titulo = "Potencia tus estudios sorbe la educacion sexual";
   subtitulo = "En colaboracion con la sociedad";
+
+  aplicarFiltro(hover: boolean) {
+    const img = document.querySelector('.card-img-top') as HTMLElement;
+    img.style.filter = hover ? 'brightness(70%)' : 'none';
+  }
 }
