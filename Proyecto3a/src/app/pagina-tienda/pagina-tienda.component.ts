14057f94549c97e57be42182da956e2c6fa0b206
jfortizb0 <juanfrancisco.ortiz.bonet@cieep.com>
2024-04-30 11:11:44 +0200

    Poner encabezado en la pagina de tienda

--- Proyecto3a/src/app/pagina-tienda/pagina-tienda.component.ts
+++ Proyecto3a/src/app/pagina-tienda/pagina-tienda.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { EncabezadoComponent } from '../encabezado/encabezado.component';
 
 @Component({
   selector: 'app-pagina-tienda',
   standalone: true,
-  imports: [],
+  imports: [EncabezadoComponent],
   templateUrl: './pagina-tienda.component.html',
   styleUrl: './pagina-tienda.component.css'
 })
 export class PaginaTiendaComponent {
-
+  titulo = "Pagina tienda";
+  subtitulo = "Inscripciones";
 }
