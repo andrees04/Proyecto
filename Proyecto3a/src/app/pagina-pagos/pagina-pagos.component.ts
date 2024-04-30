c2db73fe711139410c8a0add16d3a985bace8ae9
Julio catala <julio.catalasanchez@cieep.com>
2024-04-30 11:32:17 +0200

    Finalización pagina pagos. Vinculacion de pagina tienda con pagos

--- Proyecto3a/src/app/pagina-pagos/pagina-pagos.component.ts
+++ Proyecto3a/src/app/pagina-pagos/pagina-pagos.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { EncabezadoComponent } from '../encabezado/encabezado.component';
 
 @Component({
   selector: 'app-pagina-pagos',
   standalone: true,
-  imports: [],
+  imports: [EncabezadoComponent],
   templateUrl: './pagina-pagos.component.html',
   styleUrl: './pagina-pagos.component.css'
 })
 export class PaginaPagosComponent {
-
+  titulo = "Pagos";
+  subtitulo = "";
+  
 }
--- Proyecto3a/src/app/pagina-tienda/pagina-tienda.component.ts
+++ Proyecto3a/src/app/pagina-tienda/pagina-tienda.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { EncabezadoComponent } from '../encabezado/encabezado.component';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-pagina-tienda',
   standalone: true,
-  imports: [EncabezadoComponent],
+  imports: [EncabezadoComponent, RouterLink, RouterLinkActive],
   templateUrl: './pagina-tienda.component.html',
   styleUrl: './pagina-tienda.component.css'
 })
