7f4cb79265f669b3b80ce8ec654eb89c7ace87ab
andrees04 <blasco.ivia@gmail.com>
2024-05-08 17:27:04 +0200

    ruta pagiuna principal

--- Proyecto3a/src/app/app.routes.ts
+++ Proyecto3a/src/app/app.routes.ts
@@ -18,7 +18,7 @@ import path from 'path';
 
 export const routes: Routes = [
 
-    { path: '',   redirectTo: '/pagina-tienda', pathMatch: 'full' },
+    { path: '',   redirectTo: '/pagina-principal', pathMatch: 'full' },
 
     { path: 'pagina-principal', title:"Home", component: PaginaPrincipalComponent },
 
