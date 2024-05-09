f99b1b82ddaf51d2904efca30be2357069f82347
andrees04 <blasco.ivia@gmail.com>
2024-05-09 19:36:57 +0200

    Arreglos de colores como wave y quienes somos

--- Proyecto3a/src/app/app.routes.ts
+++ Proyecto3a/src/app/app.routes.ts
@@ -16,7 +16,7 @@ import path from 'path';
 
 export const routes: Routes = [
 
-    { path: '',   redirectTo: '/pagina-principal', pathMatch: 'full' },
+    { path: '',   redirectTo: '/pagina-quienes-somos', pathMatch: 'full' },
 
     { path: 'pagina-principal', title:"Home", component: PaginaPrincipalComponent },
 
