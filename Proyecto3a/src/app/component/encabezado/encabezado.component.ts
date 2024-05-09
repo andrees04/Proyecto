b51f409b2b7d62b664eb10020542e5a54c2b5289
jfortizb0 <juanfrancisco.ortiz.bonet@cieep.com>
2024-05-09 09:51:28 +0200

    Cambiar el color de fondo del encabezado en las paginas de formacion

--- Proyecto3a/src/app/component/encabezado/encabezado.component.ts
+++ Proyecto3a/src/app/component/encabezado/encabezado.component.ts
@@ -1,15 +1,17 @@
+import { NgIf } from '@angular/common';
 import { Component, Input, input , HostBinding } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-encabezado',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgIf],
   templateUrl: './encabezado.component.html',
   styleUrl: './encabezado.component.css'
 })
 export class EncabezadoComponent {
   @Input({required: true}) t!: string;
   @Input({required: true}) st!: string;
+  @Input({required: false}) paginaFormacion!: boolean;
   @HostBinding('attr.ngSkipHydration') skipHydration = true;
 }
